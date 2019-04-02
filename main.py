9d77a6a0f56e0e341ed749ca0286d728eb748e06
Sebastian Wiewiora <sbswwr+git@gmail.com>
2019-04-02 22:01:10 +0200

    Subprocess

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 from subprocess import check_output, PIPE, Popen
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from flask_login import current_user
+# from flask_login import current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
+
+
 
 @app.route('/')
 def index():
@@ -19,30 +21,31 @@ def index():
 def joined(msg):
     # Run "cat", which is a simple Linux program that prints it's input.
     global process
-    process = Popen(shell=True, stdin=PIPE, stdout=PIPE)
+    process = Popen('cmd', shell=True, stdin=PIPE, stdout=PIPE)
     emit('status', {'msg': 'Connected to server'})
 
 
 @socketio.on('command', namespace='/shell')
 def command(cmd):
-    command = cmd['msg']
-    emit('message', {'msg': '$ ' + command})
-    print(command)
+    message = cmd['msg']
+    emit('message', {'msg': '$ ' + message})
+    message = (message + '\r\n').encode()
+    print(message)
     try:
-        process.stdin.write(b'cd ..\n')
-        process.stdin.flush()
-        output = process.stdout.read()
+        # process.stdin.write(message.encode() + b'\n')
+        # process.stdin.flush()
+        (stdout, stderr) = process.communicate(input=message, timeout=30)
+        print(stdout)
+        print(stderr)
         exitstatus = process.poll()
         if exitstatus == 0:
-            emit('message', {'msg': output})
+            emit('message', {'msg': stdout.decode()})
         else:
-            emit('error', {'msg': output})
-
+            emit('error', {'msg': stderr.decode()})
     except Exception as err:
         result = str(err)
+        print(result)
         emit('error', {'msg': result})
-
-
 
 
 if __name__ == '__main__':
