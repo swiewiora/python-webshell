c5efcba3ff755bb4a2dd71e4afdbca4157858062
swiewiora <>
2019-04-08 21:10:23 +0200

    Added license

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(
     name='webshell',
-    version='1.0.0',
+    version='1.0',
     packages=find_packages(),
-    include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
     ],
+    author='Sebastian Wiewiora',
+    author_email='sebawiewior@gmail.com',
+    description='Flask application which allows the user to execute '
+                'arbitrary shell commands on the remote web server via '
+                'browser.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/swiewiora/python-webshell',
+    license='MIT',
 )
